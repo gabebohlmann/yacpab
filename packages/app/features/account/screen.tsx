7f9a239f6378251cc9c12ae0c58e6799a290470c
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-27 09:12:39 -0500

    Add colorscheme auto updating text

--- packages/app/features/account/screen.tsx
+++ packages/app/features/account/screen.tsx
@@ -1,11 +1,14 @@
 // packages/app/features/account/screen.tsx
 'use client'
 import { View, Text } from "react-native"
+import { useColorScheme } from "react-native"
 
 export function AccountScreen() {
+  const colorScheme = useColorScheme()
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Account Screen</Text>
+      <Text style={{ color: colorScheme === 'dark' ? 'white' : 'black' }}>Account Screen</Text>
     </View>
   )
 }
--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,11 +1,13 @@
 // packages/app/features/home/screen.tsx
 'use client'
 import { View, Text } from "react-native"
+import { useColorScheme } from "react-native"
 
 export function HomeScreen() {
+  const colorScheme = useColorScheme()
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text style={{ color: colorScheme === 'dark' ? 'white' : 'black' }}>Home Screen</Text>
     </View>
   )
 }

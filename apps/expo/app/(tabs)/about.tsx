972daa273ef723c1cc24459d2bbea61dcf67f16c
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-27 14:39:25 -0500

    Remove cli added screens and pages

--- apps/expo/app/(tabs)/about.tsx
+++
@@ -1,6 +0,0 @@
-// apps/expo/app/(tabs)/about.tsx
-import { AboutScreen } from 'app/features/about/screen';
-
-export default function AboutTabPage() {
-  return <AboutScreen />;
-}
--- apps/next/app/(tabs)/about/page.tsx
+++
@@ -1,8 +0,0 @@
-// apps/next/app/(tabs)/about/page.tsx
-'use client';
-
-import { AboutScreen } from 'app/features/about/screen';
-
-export default function AboutPage() {
-  return <AboutScreen />;
-}
